30170a7bd975150cab801dc7a5cc111a8b69454c
Antônio A E Martins <antonioaem@icloud.com>
2025-08-09 11:10:42 -0700

    chorus: change from default library agent to githubagent test

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,12 +2,12 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
-import { weatherAgent } from './agents/weather-agent';
+import { githubWorkflow } from './workflows/github-workflow';
+import { githubAgent } from './agents/github-agent';
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+  workflows: { githubWorkflow },
+  agents: { githubAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
